4468911db60d920def8d74b1ea18115f2e430681
ALIENWARE <sky-walker@sjtu.edu.cn>
2022-03-19 21:05:35 +0800

    1232

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+const int maxm=5000;
+const int maxn=3000;
+int n,m,cnt;
+struct edge{
+    int to;   //end
+    int w; //weight
+    int next;
+};
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
