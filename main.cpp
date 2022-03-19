686a5468efda9e4685c5752feccfa1c64615c6ed
ALIENWARE <sky-walker@sjtu.edu.cn>
2022-03-19 22:44:57 +0800

    1232

--- main.cpp
+++ main.cpp
@@ -1,34 +1,52 @@
 #include <iostream>
+#include <climits>
 using namespace std;
 
 const int maxm=5000;
 const int maxn=3000;
 int n,m,cnt;
 struct edge{
+    int start;
     int to;   //end
-    int w; //weight
-    int next;
+    long long w; //weight
 }edges[maxm];
-int head[maxn];
-void init(){
-    for(int i=0;i<=n;++i) head[i]=-1;
-    cnt=0;
-}
-void add_edge(int u, int v, int w){
+long long dis[maxn];
+void add_edge(int u, int v, long long w){
+    edges[cnt].start=u;
     edges[cnt].to=v;
     edges[cnt].w=w;
-    edges[cnt].next=head[u];
-    head[u]=cnt++;
+    cnt++;
+}
+void init(){
+    cnt=0;
+    for(int i=1;i<=n;++i) {
+        dis[i]=INT_MAX;
+        add_edge(0, i, 0);
+    }
+    dis[0]=0;
 }
 
 
 int main() {
-    cin>>n>>n;
+    cin>>n>>m;
     init();
-    int u,v,w;
+    int u,v;
+    long long w;
     for(int _=0;_<m;++_){
         cin>>u>>v>>w;
         add_edge(u,v,w);
     }
-
+    for(int i=0;i<n-1;++i){
+        for(int j=0;j<cnt;++j){
+            if(dis[edges[j].start]+edges[j].w<dis[edges[j].to])
+                dis[edges[j].to]=dis[edges[j].start]+edges[j].w;
+        }
+    }
+    for(int j=0;j<cnt;++j){
+        if(dis[edges[j].start]+edges[j].w<dis[edges[j].to]){
+            cout<<"Yes";
+            return 0;
+        }
+    }
+    cout<<"No";
 }
