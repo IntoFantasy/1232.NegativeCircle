c4b11e0db5f30458b13f8bdf4045127a96699f4a
ALIENWARE <sky-walker@sjtu.edu.cn>
2022-03-19 23:03:54 +0800

    Final answer

---
+++ cmake-build-debug/CMakeFiles/1232_negative_circle.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1232_negative_circle.exe"
+  "1232_negative_circle.exe.manifest"
+  "1232_negative_circle.pdb"
+  "CMakeFiles/1232_negative_circle.dir/main.cpp.obj"
+  "lib1232_negative_circle.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1232_negative_circle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
